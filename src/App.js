d5b004578c26bac239fa7756e08e91e6746d92c2
ArfinHasib <arfinhasib21375@gmail.com>
2024-04-20 12:35:15 +0600

    finalized code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Player from './component/Player';
+
+import './component/Style.css';
+
+function App() {
+   return (
+      <div className='App'>
+         <Player />
+      </div>
+   );
+}
+
+export default App;
